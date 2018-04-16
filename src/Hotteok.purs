5b387232dd3169cd9df42f19aba6380f27525fcc
justinwoo <moomoowoo@gmail.com>
2018-04-16 10:29:34 +0300

    add matchJS Union on tuple pairs of (guard, fn)

--- src/Hotteok.purs
+++ src/Hotteok.purs
@@ -1,9 +1,14 @@
 module Hotteok where
 
+import Prelude
+
 import Data.Either (Either(..))
 import Data.Foreign (Foreign, toForeign)
-import Type.Prelude (class RowToList, SProxy)
-import Type.Row (Cons, Nil)
+import Data.Maybe (Maybe(..))
+import Data.Record as Record
+import Data.Tuple (Tuple(..))
+import Type.Prelude (class IsSymbol, class RowToList, RLProxy(RLProxy), SProxy(SProxy))
+import Type.Row (Cons, Nil, kind RowList)
 import Unsafe.Coerce (unsafeCoerce)
 
 data JSUnion (members :: # Type)
@@ -46,3 +51,35 @@ fromMember
   -> ty
   -> JSUnion members
 fromMember _ x = unsafeCoerce x
+
+matchJSUnion
+  :: forall members xs pairs result
+   . RowToList members xs
+  => MatchMembers xs members pairs result
+  => { | pairs }
+  -> JSUnion members
+  -> Maybe result
+matchJSUnion =
+  matchMembers (RLProxy :: RLProxy xs)
+
+class MatchMembers (xs :: RowList) (members :: # Type) (pairs :: # Type) result
+  | xs result -> members pairs
+  where
+    matchMembers :: RLProxy xs -> { | pairs } -> JSUnion members -> Maybe result
+
+instance matchMembersNil :: MatchMembers Nil members pairs result where
+  matchMembers _ _ _ = Nothing
+
+instance matchMembersCons ::
+  ( IsSymbol name
+  , RowCons name ty members' members
+  , RowCons name (Tuple (UnsafeGuardFor name ty) (ty -> result)) pairs' pairs
+  , MatchMembers tail members pairs result
+  ) => MatchMembers (Cons name ty tail) members pairs result where
+  matchMembers _ pairs union =
+    case unsafeGuardMember unsafeGuard union of
+      Right x -> Just $ fn x
+      Left _ -> matchMembers (RLProxy :: RLProxy tail) pairs union
+    where
+      nameP = SProxy :: SProxy name
+      Tuple unsafeGuard fn = Record.get nameP pairs
--- test/Main.purs
+++ test/Main.purs
@@ -7,12 +7,15 @@ import Control.Monad.Eff.Console (log)
 import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
 import Control.Monad.Except (runExcept)
 import Data.Either (Either(..), isRight)
-import Data.Foreign (readInt)
+import Data.Foreign (readInt, readString)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
 import Hotteok as H
 import Test.Unit as T
 import Test.Unit.Assert as Assert
 import Test.Unit.Main as TM
 import Type.Prelude (SProxy(..))
+import Unsafe.Coerce (unsafeCoerce)
 
 type TestUnion = H.JSUnion
   ( name :: String
@@ -28,6 +31,13 @@ countGuard =
       $ isRight
     <<< runExcept
     <<< readInt
+
+nameGuard :: H.UnsafeGuardFor "name" String
+nameGuard =
+  H.UnsafeGuardFor
+     $ isRight
+   <<< runExcept
+   <<< readString
 
 main :: Eff () Unit
 main = do
@@ -72,6 +82,35 @@ main = do
         Left e ->
           T.failure "incorrect branch from JSUnion"
 
+    T.test "matchMembers 1" do
+      let
+        (union :: TestUnion) = H.fromMember countP 1
+        match = H.matchJSUnion
+          { count: Tuple countGuard show
+          , name: Tuple nameGuard id
+          }
+          union
+      case match of
+        Just value -> do
+          Assert.equal value "1"
+        Nothing ->
+          T.failure "incorrect result from matchJSUnion"
+
+    T.test "matchMembers 2" do
+      let
+        (union :: TestUnion) = unsafeCoerce { crap: "some bullshit from JS" }
+        match = H.matchJSUnion
+          { count: Tuple countGuard show
+          , name: Tuple nameGuard id
+          }
+          union
+      case match of
+        Just value -> do
+          T.failure "incorrect result from matchJSUnion"
+        Nothing ->
+          T.success
+
+
 chalkMain :: Eff () Unit
 chalkMain = unsafeCoerceEff do
   let
@@ -88,4 +127,4 @@ foreign import chalk
             ( fn :: String -> String
             , obj :: { bgYellow :: String -> String }
             )
-     } 
+     }
